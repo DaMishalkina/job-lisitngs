61a1ac49225297ee843a06697fd1434fe347bb26
Daria Mishalkina <dashaio251@gmail.com>
2024-03-12 20:47:31 +0100

    pagination

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -5,10 +5,13 @@ import {computed, onMounted, ref, watch} from "vue";
 import {getTags} from "@/utils/getTags";
 import TagsList from "@/components/TagsList.vue";
 
+const ITEMS_NUMBER = 4;
 
 const props = defineProps<{
   jobs: Job[]
 }>()
+
+const currentPage = ref<number>(1);
 
 const modifiedJobs = computed(() => {
   return props.jobs.map((job) => {
@@ -24,11 +27,35 @@ const filteredJobs =  computed(() => {
   if(selectedTags.value.size === 0){
     return modifiedJobs.value;
   }
-  return modifiedJobs.value.filter((job) => {
+  return  modifiedJobs.value.filter((job) => {
     return [...selectedTags.value].every(selectedTag => {
       return job.tags.includes(selectedTag as string)
     })
   });
+
+})
+
+const endIndex = computed(() => {
+  return currentPage.value * ITEMS_NUMBER;
+});
+
+const startIndex = computed(() => {
+  return (currentPage.value - 1) * ITEMS_NUMBER;
+})
+
+const hasNextPage = computed(() => {
+  return filteredJobs.value.length > endIndex.value;
+})
+
+const paginatedJobs = computed(() => {
+  return filteredJobs.value.slice(startIndex.value, endIndex.value);
+})
+
+const pageStateOptions = computed(() => {
+  return {
+    filter: [...selectedTags.value].join(","),
+    page: currentPage.value
+  }
 })
 
 const onSelect = (clickedTag: string) => {
@@ -36,6 +63,15 @@ const onSelect = (clickedTag: string) => {
     selectedTags.value.add(clickedTag);
   } else {
     selectedTags.value.delete(clickedTag);
+  }
+}
+
+const handlePageClick = (event: Event) => {
+  const buttonId = (event?.target as HTMLElement)?.id;
+  if(buttonId === "prev"){
+    currentPage.value =  currentPage.value - 1
+  } else {
+    currentPage.value = currentPage.value + 1
   }
 }
 
@@ -49,26 +85,66 @@ onMounted(() => {
     selectedTags.value.add(filter)
   })
 
+  currentPage.value = Number(windowData?.page);
+
+
 })
 
 watch(() => selectedTags.value.size, () => {
-  if(selectedTags.value.size > 0) {
-    window.history.pushState(null, document.title, `${window.location.pathname}?filter=${[...selectedTags.value].join(",")}`)
+  currentPage.value = 1;
+
+
+  // if(selectedTags.value.size > 0) {
+  //   window.history.pushState(null, document.title, `${window.location.pathname}?filter=${[...selectedTags.value].join(",")}`)
+  // } else {
+  //   window.history.pushState(null, document.title, `${window.location.pathname}`)
+  // }
+})
+
+
+
+watch(() => pageStateOptions.value, (value) => {
+  const searchURL = new URL(window.location.href);
+  if(value.filter.length > 0) {
+    searchURL.searchParams.set("filter", value.filter);
   } else {
-    window.history.pushState(null, document.title, `${window.location.pathname}`)
+    searchURL.searchParams.delete("filter");
   }
+  if(value.page > 0){
+    searchURL.searchParams.set("page", String(value.page));
+  } else {
+    searchURL.searchParams.delete("page");
+  }
+
+  window.history.pushState({}, '', searchURL);
 })
 </script>
 
 <template>
+  <div>
+    <button
+        id="prev"
+        @click="handlePageClick"
+        v-if="currentPage > 1"
+    >
+      Prev
+    </button>
+    <button
+        id="next"
+        @click="handlePageClick"
+        v-if="hasNextPage"
+    >
+      Next
+    </button>
+  </div>
+
   <section v-if="selectedTags?.size > 0">
     <tags-list :tags="[...selectedTags.values()]" @on-tag-click="onSelect" />
     <button @click="onClear()">Clear</button>
   </section>
-  <section v-if="filteredJobs?.length">
+  <section v-if="paginatedJobs?.length">
     <ul>
-      <job-card v-for="job in filteredJobs" :key="job.id" :job="job" @on-tag-select="onSelect" />
+      <job-card v-for="job in paginatedJobs" :key="job.id" :job="job" @on-tag-select="onSelect" />
     </ul>
   </section>
-
 </template>
